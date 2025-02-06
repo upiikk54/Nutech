09da0bbaa3ad76daf23b4463b884e14e0d99f673
praditya luthfi <pradityaluthfi45@gmail.com>
2025-02-06 21:47:30 +0700

    feat: revision

--- config/config.js
+++ config/config.js
@@ -6,20 +6,20 @@ module.exports = {
     password: process.env.POSTGRES_PASSWORD,
     database: process.env.POSTGRES_DATABASE,
     host: process.env.POSTGRES_HOST,
-    dialect: process.env.POSTGRES_USER
+    dialect: process.env.POSTGRES_DIALECT
   },
   test: {
     username: process.env.POSTGRES_USER,
     password: process.env.POSTGRES_PASSWORD,
     database: process.env.POSTGRES_DATABASE,
     host: process.env.POSTGRES_HOST,
-    dialect: process.env.POSTGRES_USER
+    dialect: process.env.POSTGRES_DIALECT
   },
   production: {
     username: process.env.POSTGRES_USER,
     password: process.env.POSTGRES_PASSWORD,
     database: process.env.POSTGRES_DATABASE,
     host: process.env.POSTGRES_HOST,
-    dialect: process.env.POSTGRES_USER
+    dialect: process.env.POSTGRES_DIALECT
   }
 }
